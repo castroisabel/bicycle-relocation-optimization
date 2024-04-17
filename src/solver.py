3f19c8cab2f75b0943c3101e72588d592f923859
Isabel Cordeiro <isabel.cordeiro@hotmart.com>
2024-04-17 12:31:15 -0300

    task 1

---
+++ src/solver.py
@@ -0,0 +1,54 @@
+import pyomo.environ as pyEnv
+
+def relocate_bicycles(matrix, a, s, T):
+    model = pyEnv.ConcreteModel()
+
+    # índices para as áreas, categorias e bicicletas
+    model.I = pyEnv.Set(initialize=range(matrix.shape[0]))
+    model.J = pyEnv.Set(initialize=range(matrix.shape[1]))
+    model.K = pyEnv.Set(initialize=range(matrix.shape[2]))
+
+    # variáveis de decisão
+    model.x = pyEnv.Var(model.I, model.J, model.K, domain=pyEnv.Binary, initialize=0)
+
+    # função objetivo
+    model.profit = pyEnv.Objective(
+        expr=sum(matrix[i, j, k] * model.x[i, j, k] for i in model.I for j in model.J for k in model.K),
+        sense=pyEnv.maximize
+    )
+
+    # restrições
+    def supply_constraint(model, j):
+        return sum(model.x[i, j, k] for i in model.I for k in model.K) <= a[j]
+
+    model.supply_constraint = pyEnv.Constraint(model.J, rule=supply_constraint)
+
+    def capacity_constraint(model):
+        return sum(s[j] * model.x[i, j, k] for i in model.I for j in model.J for k in model.K) <= T
+
+    model.capacity_constraint = pyEnv.Constraint(rule=capacity_constraint)
+
+    # adicionar restrição condicional
+    def sequential_constraint_rule(model, i, j, k):
+        if k == 0:
+            return pyEnv.Constraint.Skip
+        else:
+            return model.x[i, j, k] <= model.x[i, j, k-1]
+
+    model.sequential_constraint = pyEnv.Constraint(model.I, model.J, model.K, rule=sequential_constraint_rule)
+
+    # resolver o modelo
+    solver = pyEnv.SolverFactory('glpk')
+    results = solver.solve(model)
+
+    print("Status:", results.solver.termination_condition)
+    print("Valor objetivo ótimo (Lucro esperado):", pyEnv.value(model.profit))
+
+    print("\nSolução ótima:")
+    sum_x = [[sum(model.x[i, j, k].value for k in model.K) for j in model.J] for i in model.I]
+    for i in model.I:
+        for j in model.J:
+            if sum_x[i][j] != 0:
+                print(f"Número de bicicletas da categoria {j + 1} a serem movidas para a área {i + 1}: {sum_x[i][j]}")
+
+    return pyEnv.value(model.profit), sum_x
